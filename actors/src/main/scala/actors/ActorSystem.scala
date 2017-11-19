7b3eb5f50334572567f860896cfc1f88b71d55b0
Romain Ruetschi <romain.ruetschi@gmail.com>
2017-11-19 21:35:46 +0100

    Refactor library

---
+++ actors/src/main/scala/actors/ActorSystem.scala
@@ -0,0 +1,45 @@
+package actors
+
+import stainless.lang._
+import stainless.collection._
+import stainless.annotation._
+
+import scala.language.postfixOps
+
+case class ActorSystem(
+  behaviors: CMap[ActorRef, Behavior],
+  inboxes: CMap[(ActorRef, ActorRef), List[Msg]]
+) {
+
+  def step(from: ActorRef, to: ActorRef): ActorSystem = {
+    inboxes(from -> to) match {
+      case Nil() =>
+        this
+
+      case Cons(msg, msgs) =>
+        val (newBehavior, toSend) = deliverMessage(to, msg)
+
+        val newBehaviors = behaviors.updated(to, newBehavior)
+        val newInboxes = toSend.foldLeft(inboxes.updated(from -> to, msgs)) {
+          case (acc, Packet(dest, m)) => acc.updated(to -> dest, acc(to -> dest) :+ m)
+        }
+
+        ActorSystem(newBehaviors, newInboxes)
+    }
+  }
+
+  def deliverMessage(actor: ActorRef, msg: Msg): (Behavior, List[Packet]) = {
+    val behavior = behaviors(actor)
+
+    val ctx = ActorContext(actor, Nil())
+    val nextBehavior = behavior.processMsg(msg)(ctx)
+
+    (nextBehavior, ctx.toSend)
+  }
+
+  def isStopped(id: ActorRef): Boolean = {
+    behaviors(id) == Behavior.stopped
+  }
+
+}
+
---
+++ actors/src/main/scala/actors/package.scala
@@ -0,0 +1,31 @@
+
+import stainless.lang._
+import stainless.collection._
+import stainless.annotation._
+
+import scala.language.postfixOps
+
+package object actors {
+
+  abstract class Msg
+
+  case class Packet(dest: ActorRef, payload: Msg)
+
+  case class ActorContext(self: ActorRef, var toSend: List[Packet]) {
+
+    def send(to: ActorRef, msg: Msg): Unit = {
+      toSend = toSend :+ Packet(to, msg)
+    }
+
+  }
+
+  abstract class ActorRef {
+
+    def !(msg: Msg)(implicit ctx: ActorContext): Unit = {
+      ctx.send(this, msg)
+    }
+
+  }
+
+}
+
