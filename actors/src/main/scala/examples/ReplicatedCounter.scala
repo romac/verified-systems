921585ab3fe74b8230cff949819e0050bcaf50c0
Romain Ruetschi <romain.ruetschi@gmail.com>
2017-11-20 09:44:35 +0100

    Actually replicate the node

--- actors/src/main/scala/examples/ReplicatedCounter.scala
+++ actors/src/main/scala/examples/ReplicatedCounter.scala
@@ -50,13 +50,39 @@ object replicated {
   case class Counting(counter: Counter) extends Behavior {
     def processMsg(msg: Msg)(implicit ctx: ActorContext): Behavior = msg match {
       case Inc => Counting(counter.increment)
+      case _ => Behavior.same
     }
   }
 
-  case class Replicated(underlying: Behavior, replica: ActorRef) extends Behavior {
-    def processMsg(msg: Msg)(implicit ctx: ActorContext): Behavior = {
-      replica ! msg
-      Replicated(underlying.processMsg(msg), replica)
+  case class Replicated(underlying: Behavior, queue: List[Msg], replica: ActorRef) extends Behavior {
+    def processMsg(msg: Msg)(implicit ctx: ActorContext): Behavior = msg match {
+      case Replicate(_) =>
+        Behavior.same
+
+      case Ack =>
+        val newBehavior = underlying.processMsg(queue.head)
+        val newQueue = queue.tail
+        if (newQueue.nonEmpty) {
+          replica ! Replicate(msg)
+        }
+        Replicated(underlying, newQueue, replica)
+
+      case msg =>
+        val newQueue = queue :+ msg
+        if (newQueue.size == 1) {
+          replica ! Replicate(msg)
+        }
+        Replicated(underlying, newQueue, replica)
+    }
+  }
+
+  case class Replica(underlying: Behavior, primary: ActorRef) extends Behavior {
+    def processMsg(msg: Msg)(implicit ctx: ActorContext): Behavior = msg match {
+      case Replicate(msg) =>
+        primary ! Ack
+        underlying.processMsg(msg)
+
+      case _ => Behavior.same
     }
   }
 
@@ -64,13 +90,27 @@ object replicated {
   case object Backup  extends ActorRef
 
   case object Inc extends Msg
+  case class Replicate(msg: Msg) extends Msg
+
+  case object Ack extends Msg
+
+  def repIncMsgs(inbox: List[Msg]): List[Msg] = inbox.filter {
+    case Replicate(Inc) => true
+    case _ => false
+  }
+
+  def incMsgs(inbox: List[Msg]): List[Msg] = inbox.filter {
+    case Inc => true
+    case _ => false
+  }
 
   def invariant(s: ActorSystem): Boolean = {
     s.inboxes(Backup -> Backup).isEmpty &&
-    s.inboxes(Backup -> Primary).isEmpty && {
+    s.inboxes(Primary -> Primary).isEmpty && {
       (s.behaviors(Primary), s.behaviors(Backup)) match {
-        case (Replicated(Counting(p), Backup), Counting(b)) =>
-          p.value == b.value + s.inboxes(Primary -> Backup).length
+        case (Replicated(Counting(p), queue, Backup), Replica(Counting(b), Primary)) =>
+          val pendingInc = repIncMsgs(s.inboxes(Primary -> Backup)).length + incMsgs(queue).length
+          p.value == b.value + pendingInc
 
         case _ => false
       }
