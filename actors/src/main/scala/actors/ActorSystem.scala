094b35eb88c4825d54b5ec008e1136a9916e2dae
Romain Ruetschi <romain.ruetschi@gmail.com>
2017-11-20 11:33:43 +0100

    Add traces

--- actors/src/main/scala/actors/ActorSystem.scala
+++ actors/src/main/scala/actors/ActorSystem.scala
@@ -3,13 +3,27 @@ package actors
 import stainless.lang._
 import stainless.collection._
 import stainless.annotation._
+import stainless.util.Random
+import stainless.util.Random.State
 
 import scala.language.postfixOps
 
 case class ActorSystem(
   behaviors: CMap[ActorRef, Behavior],
-  inboxes: CMap[(ActorRef, ActorRef), List[Msg]]
+  inboxes: CMap[(ActorRef, ActorRef), List[Msg]],
+  trace: List[Transition]
 ) {
+
+  @library
+  def run(actors: List[ActorRef], steps: BigInt)(implicit state: State): ActorSystem = {
+    require(actors.nonEmpty && steps >= 0)
+
+    val (a, b) = (Random.nextBigInt(actors.size), Random.nextBigInt(actors.size))
+    val (from, to) = (actors(a), actors(b))
+    val next = step(from, to)
+    if (steps == 0) next
+    else next.run(actors, steps - 1)
+  }
 
   def step(from: ActorRef, to: ActorRef): ActorSystem = {
     inboxes(from -> to) match {
@@ -17,28 +31,52 @@ case class ActorSystem(
         this
 
       case Cons(msg, msgs) =>
-        val (newBehavior, toSend) = deliverMessage(to, msg)
+        val (newBehavior, toSend, trans) = deliverMessage(to, from, msg)
 
         val newBehaviors = behaviors.updated(to, newBehavior)
         val newInboxes = toSend.foldLeft(inboxes.updated(from -> to, msgs)) {
           case (acc, Packet(dest, m)) => acc.updated(to -> dest, acc(to -> dest) :+ m)
         }
 
-        ActorSystem(newBehaviors, newInboxes)
+        ActorSystem(newBehaviors, newInboxes, trace ++ trans)
     }
   }
 
-  def deliverMessage(actor: ActorRef, msg: Msg): (Behavior, List[Packet]) = {
-    val behavior = behaviors(actor)
+  def deliverMessage(to: ActorRef, from: ActorRef, msg: Msg): (Behavior, List[Packet], List[Transition]) = {
+    val behavior = behaviors(to)
 
-    val ctx = ActorContext(actor, Nil())
+    val ctx = ActorContext(to, Nil())
     val nextBehavior = behavior.processMsg(msg)(ctx)
 
-    (nextBehavior, ctx.toSend)
+    val trans = List(
+      // Transition.receive(from, to, msg),
+      Transition.become(to, nextBehavior)
+    ) ++ ctx.toSend.map {
+      case Packet(dest, msg) => Transition.send(to, dest, msg)
+    }
+
+    (nextBehavior, ctx.toSend, trans)
   }
 
   def isStopped(id: ActorRef): Boolean = {
     behaviors(id) == Behavior.stopped
+  }
+
+  def send(to: ActorRef, msg: Msg): ActorSystem = {
+    val inbox = inboxes(Main -> to) :+ msg
+    ActorSystem(behaviors, inboxes.updated(Main -> to, inbox), trace)
+  }
+
+  def transition(trans: Transition): ActorSystem = trans match {
+    case Become(ref, behavior) =>
+      ActorSystem(behaviors.updated(ref, behavior), inboxes, trace :+ trans)
+
+    case Send(from, to, msg) =>
+      val inbox = inboxes(from -> to) :+ msg
+      ActorSystem(behaviors, inboxes.updated(from -> to, inbox), trace :+ trans)
+
+    case Receive(from, to, msg) =>
+      ActorSystem(behaviors, inboxes, trace :+ trans)
   }
 
 }
--- actors/src/main/scala/examples/ReplicatedCounter.scala
+++ actors/src/main/scala/examples/ReplicatedCounter.scala
@@ -5,6 +5,7 @@ import stainless.lang._
 import stainless.proof._
 import stainless.collection._
 import stainless.annotation._
+import stainless.util.Random
 
 import scala.language.postfixOps
 
@@ -48,7 +49,10 @@ object replicated {
   }
 
   def invariant(s: ActorSystem): Boolean = {
-    s.inboxes((Backup(), Backup())).isEmpty && {
+    s.behaviors(Main) == Behavior.stopped &&
+    s.inboxes(Main -> Main).isEmpty &&
+    s.inboxes(Main -> Backup()).isEmpty &&
+    s.inboxes(Backup() -> Backup()).isEmpty && {
       (s.behaviors(Primary()), s.behaviors(Backup())) match {
         case (PrimBehav(p), BackBehav(b)) =>
           p.value == b.value + s.inboxes(Primary() -> Backup()).length
@@ -63,5 +67,51 @@ object replicated {
     val newSystem = s.step(from, to)
     invariant(newSystem)
   } holds
+
+  @extern
+  def main(args: Array[String]): Unit = {
+    val actors = List[ActorRef](Main, Primary(), Backup())
+    val behaviors = CMap[ActorRef, Behavior] {
+      case Main      => Behavior.stopped
+      case Primary() => PrimBehav(Counter(0))
+      case Backup()  => BackBehav(Counter(0))
+    }
+
+    val inboxes = CMap[(ActorRef, ActorRef), List[Msg]](_ => Nil())
+
+    val init = ActorSystem(behaviors, inboxes, Nil())
+    implicit val state = Random.newState
+
+    val res = init
+      .send(Primary(), Inc())
+      .send(Primary(), Inc())
+      .send(Primary(), Inc())
+      .run(actors, 100)
+
+    val (holds, _) = invHolds(init, res.trace) { s =>
+      s.behaviors(Main) == Behavior.stopped &&
+      s.inboxes(Main -> Main).isEmpty &&
+      s.inboxes(Main -> Backup()).isEmpty &&
+      s.inboxes(Backup() -> Backup()).isEmpty && {
+        (s.behaviors(Primary()), s.behaviors(Backup())) match {
+          case (PrimBehav(p), BackBehav(b)) =>
+            p.value >= b.value
+
+          case _ => false
+        }
+      }
+    }
+
+    println(holds)
+  }
+
+  def invHolds(init: ActorSystem, trace: List[Transition])(inv: ActorSystem => Boolean): (Boolean, ActorSystem) = {
+    require(inv(init))
+
+    trace.foldLeft((inv(init), init)) { case ((holds, sys), trans) =>
+      val next = sys.transition(trans)
+      (holds && inv(next), next)
+    }
+  }
 
 }
